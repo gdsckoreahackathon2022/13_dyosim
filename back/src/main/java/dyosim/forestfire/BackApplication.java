8df11d359c5539b9cb676d72f99761de93a14be4
gidskql6671 <gidskql6671@gmail.com>
2022-02-04 21:59:19 +0900

    [Feat] Open API를 호출을 스케쥴러로 관리

--- back/src/main/java/dyosim/forestfire/BackApplication.java
+++ back/src/main/java/dyosim/forestfire/BackApplication.java
@@ -3,7 +3,9 @@ package dyosim.forestfire;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @PropertySource("classpath:credential.properties")
 public class BackApplication {
