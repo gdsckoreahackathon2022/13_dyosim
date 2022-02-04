ebef7f944f4c335b605157185aa70bc2a026822a
gidskql6671 <gidskql6671@gmail.com>
2022-02-04 21:57:55 +0900

    [Test] Open API를 호출하는 기능 테스팅

--- back/src/main/java/dyosim/forestfire/parse/openapi/FFResponse.java
+++ back/src/main/java/dyosim/forestfire/parse/openapi/FFResponse.java
@@ -3,12 +3,14 @@ package dyosim.forestfire.parse.openapi;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Builder
+@ToString
 public class FFResponse {
 	private LocalDateTime date;
 	private int regionCode;
